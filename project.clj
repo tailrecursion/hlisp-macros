7623f431be845be845456d17de2179e0dc7b4101
Micha Niskin <asdf@ubergibson.com>
2012-10-06 21:04:56 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hlisp-macros "0.1.0-SNAPSHOT"
+  :description "Some macros for use in hlisp projects."
+  :url "http://github.com/micha/hlisp-macros"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
