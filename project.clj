06dab10cf0139f2dcd73ec2e0de53066c205eb90
Micha Niskin <asdf@ubergibson.com>
2013-03-08 17:40:48 -0500

    move to tr in clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject hlisp-macros "1.0.0"
+(defproject tailrecursion/hlisp-macros "1.0.0"
   :description "Some macros for use in hlisp projects."
-  :url "http://github.com/micha/hlisp-macros"
+  :url "http://github.com/tailrecursion/hlisp-macros"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
